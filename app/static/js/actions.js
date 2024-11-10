e418adea92de8ea87edb168c9427d5434b43abc9
Vitaliy Sviatskiy <vitaliy.sviatskiy@tabby.ai>
2024-11-10 23:14:50 +0400

    init commit

--- app/static/js/actions.js
+++ app/static/js/actions.js
@@ -1,28 +1,28 @@
-
 function saveExistingAssignment(assignmentId) {
-    const formData = new FormData(document.getElementById("assignment-form"));
-    const data = Object.fromEntries(formData);
+    // Получаем данные из JSON Editor
+    const data = editor.getValue();
 
-    fetch(`/assignment/${assignmentId}`, {  // Use the assignmentId variable here
+    fetch(`/assignment/${assignmentId}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-    }).then(response => {
+    })
+    .then(response => {
         if (response.ok) {
             alert("Assignment saved successfully!");
             window.location.href = `/assignment/${assignmentId}`;  // Redirect to the same assignment page
         } else {
             alert("Error saving assignment!");
         }
-    }).catch(error => alert("Error: " + error));
+    })
+    .catch(error => alert("Error: " + error));
 }
 
-
 function createNewAssignment() {
-    const formData = new FormData(document.getElementById("assignment-form"));
-    const data = Object.fromEntries(formData);
+    // Получаем данные из JSON Editor
+    const data = editor.getValue();
 
     fetch(`/assignment/new`, {
         method: 'POST',
@@ -30,7 +30,8 @@ function createNewAssignment() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-    }).then(response => {
+    })
+    .then(response => {
         if (response.ok) {
             response.json().then(data => {
                 alert("New assignment created successfully!");
@@ -40,11 +41,10 @@ function createNewAssignment() {
         } else {
             alert("Error creating assignment!");
         }
-    }).catch(error => alert("Error: " + error));
+    })
+    .catch(error => alert("Error: " + error));
 }
 
-
-// Unified delete function
 function deleteAssignment(assignmentId) {
     const confirmation = confirm("Are you sure you want to delete this assignment?");
     if (confirmation) {
@@ -68,7 +68,6 @@ function deleteAssignment(assignmentId) {
     }
 }
 
-// Print function (currently just prints the page)
-function printAssignment(id) {
+function printAssignment() {
     window.print();
 }

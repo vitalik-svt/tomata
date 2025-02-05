8dc647cd75b47232f863a222b7ef7e49ebe0ff3d
vitalik-svt <vitalik.svt@gmail.com>
2025-02-06 01:15:16 +0400

    added schema service path and buttons

--- app/core/routes/service.py
+++ app/core/routes/service.py
@@ -2,45 +2,31 @@ import json
 import datetime as dt
 import logging
 
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 from fastapi.templating import Jinja2Templates
+from fastapi import Query
 from starlette.requests import Request
-from fastapi.responses import JSONResponse, HTMLResponse, PlainTextResponse
 from motor.motor_asyncio import AsyncIOMotorCollection
 
 import app.core.services.database as db
 from app.core.services.auth import get_current_user, require_authenticated_user
 from app.core.services.assignment import get_assignment_ui_schema_from_actual_model
 from app.core.models.user import UserInDB
+from app.core.models.assignment import AssignmentInUI
 from app.settings import settings
 
 
 prefix = 'service'
 router = APIRouter(prefix=f'/{prefix}')
 logger = logging.getLogger(__name__)
-
-
-@router.get("/schema/model/{assignment_class_name}")
-async def get_assignment_route(
-        assignment_class_name: str,
-        current_user: UserInDB = Depends(require_authenticated_user)
-    ):
-    assignment_ui_schema, assignment_ui_schema_hash, events_mapper = await get_assignment_ui_schema_from_actual_model(assignment_class_name)
-    return PlainTextResponse(
-        content=f"""{assignment_class_name} schema:{'\n'*2}
-Hash: {assignment_ui_schema_hash}
-{'\n'*2}
-{assignment_ui_schema}
-{'\n'*2}
-{events_mapper}
-""",
-        media_type="application/json"
-    )
+templates = Jinja2Templates(directory="app/templates")
 
 
 @router.get("/schema/{assignment_id}")
 async def get_assignment_route(
+        request: Request,
         assignment_id: str,
+        model_class_name: str = Query(AssignmentInUI.__name__, alias="model"),
         current_user: UserInDB = Depends(require_authenticated_user),
         collection: AsyncIOMotorCollection = Depends(db.collection_dependency(settings.app_assignments_collection)),
     ):
@@ -54,15 +40,18 @@ async def get_assignment_route(
     assignment_data['assignment_ui_schema'] = 'PLACEHOLDER'
     assignment_data['events_mapper'] = 'PLACEHOLDER'
 
-    return PlainTextResponse(
-        content=f"""assignment_id {assignment_data['_id']} schema:{'\n'*2}
-Hash: {assignment_ui_schema_hash}
-{'\n'*2}
-{assignment_ui_schema}
-{'\n'*2}
-{events_mapper}
-{'\n'*2}
-{json.dumps(assignment_data, indent=4, ensure_ascii=False)}
-""",
-        media_type="application/json"
-    )
+    assignment_ui_schema_cls, assignment_ui_schema_hash_cls, events_mapper_cls = await get_assignment_ui_schema_from_actual_model(model_class_name)
+
+    return templates.TemplateResponse(f"{prefix}/schema.html", {
+        "request": request,
+        "current_user": current_user,
+        "assignment_id": assignment_data['_id'],
+        "assignment_ui_schema_hash": assignment_ui_schema_hash,
+        "assignment_ui_schema": assignment_ui_schema,
+        "events_mapper": events_mapper,
+        "assignment_data": json.dumps(assignment_data, indent=4, ensure_ascii=False),
+        "model_class_name": model_class_name,
+        "assignment_ui_schema_hash_cls": assignment_ui_schema_hash_cls,
+        "assignment_ui_schema_cls": assignment_ui_schema_cls,
+        "events_mapper_cls": events_mapper_cls
+    })
